15c55f699e8df784caa3bc547759f0c2b7560d81
Ahmet Safa Orhan <ahmetsafaorhan@gmail.com>
2021-04-25 11:29:56 +0300

    Create login screen UI

---
+++ app/src/main/java/school/cactus/succulentshop/LoginActivity.kt
@@ -0,0 +1,13 @@
+package school.cactus.succulentshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.title = getString(R.string.log_in)
+    }
+}
